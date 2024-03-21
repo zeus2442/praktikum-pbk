d255f076c0fef092a4eed2fefca04d9c10279c19
zeus2442 <fauzi.rgt128@gmail.com>
2024-03-21 15:21:43 +0700

    first commit

---
+++ pertemuan1/vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>hello world</h1>
+
+    <Buttonc />
+    <Buttonc />
+
+    <TextHeader />
+  </div>
+</template>
+
+<script setup>
+import Buttonc from "./components/Buttonc.vue";
+import TextHeader from "./components/TextHeader.vue";
+</script>
---
+++ pertemuan1/vue-project/src/components/Buttonc.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="btn">submit</button>
+</template>
+
+<!--css vue 3-->
+<style scoped>
+.btn {
+  background-color: brown;
+  color: white;
+  font-size: 28px;
+}
+</style>
